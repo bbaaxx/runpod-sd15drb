8f4f09502c18e40bcd96f1418303ee4e537bd456
Ed Mosqueda (@bbaaxx) <bbaaxx@gmail.com>
2023-02-13 18:44:06 -0600

    Fix extensions paths

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,7 @@ RUN apt-get update --yes && apt-get upgrade --yes  &&  apt-get install --yes \
     apt-get clean && rm -rf /var/lib/apt/lists/* && echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 RUN useradd -m -s /bin/bash poduser && usermod -aG sudo poduser && echo "poduser ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/poduser && chmod 044 /etc/sudoers.d/poduser
 COPY --from=build-base --chown=poduser:poduser /sdui /
+
 ADD webui-user.template /sdui/stable-diffusion-webui/webui-user.sh
 ADD relauncher-webui.py /sdui/stable-diffusion-webui/relauncher.py
 ADD relauncher-invoke.py /sdui/invoke/relauncher.py
--- build_apps.sh
+++ build_apps.sh
@@ -3,17 +3,17 @@ echo "Starting To Build Dependencies"
 
 # Install packages
 apt-get update --yes && apt-get upgrade --yes && \ 
-    apt-get install --yes wget curl git libgl1 libglib2.0-0 python3-pip python-is-python3 python3-venv
-apt-get clean && rm -rf /var/lib/apt/lists/* && echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+apt-get install --yes wget curl git libgl1 libglib2.0-0 python3-pip python-is-python3 python3-venv
+apt-get clean && rm -rf /var/lib/apt/lists/* && echo "en_US.UTF-8 UTF-8" >/etc/locale.gen
 
 # Create Mount point (on persistent storage)
-mkdir -p /sdui 
+mkdir -p /sdui
 
 # Install InvokeAI
 mkdir -p /sdui/invoke
-cd /sdui/invoke 
-python -m venv .venv --prompt InvokeAI 
-source .venv/bin/activate 
+cd /sdui/invoke
+python -m venv .venv --prompt InvokeAI
+source .venv/bin/activate
 pip install InvokeAI[xformers] --use-pep517 --extra-index-url https://download.pytorch.org/whl/cu117
 deactivate
 
@@ -25,18 +25,16 @@ python -m venv venv --prompt WebUi
 source venv/bin/activate
 pip install -r requirements.txt
 ## Install extensions
-git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui-promptgen.git
-git clone https://github.com/AlUlkesh/stable-diffusion-webui-images-browser.git
-git clone https://github.com/camenduru/sd-civitai-browser
-git clone https://github.com/camenduru/stable-diffusion-webui-huggingface
-git clone https://github.com/deforum-art/deforum-for-automatic1111-webui.git
-git clone https://github.com/camenduru/sd-webui-additional-networks
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui-promptgen.git /sdui/stable-diffusion-webui/extensions/stable-diffusion-webui-promptgen
+git clone https://github.com/AlUlkesh/stable-diffusion-webui-images-browser.git /sdui/stable-diffusion-webui/extensions/stable-diffusion-webui-images-browser
+git clone https://github.com/camenduru/sd-civitai-browser.git /sdui/stable-diffusion-webui/extensions/sd-civitai_browser
+git clone https://github.com/camenduru/stable-diffusion-webui-huggingface.git /sdui/stable-diffusion-webui/extensions/stable-diffusion-webui-huggingface
+git clone https://github.com/deforum-art/deforum-for-automatic1111-webui.git /sdui/stable-diffusion-webui/extensions/deforum-for-automatic1111-webui
+git clone https://github.com/camenduru/sd-webui-additional-networks.git /sdui/stable-diffusion-webui/extensions/sd-webui-additional-networks
 ### Install dreambooth extension
-git clone https://github.com/d8ahazard/sd_dreambooth_extension.git
+git clone https://github.com/d8ahazard/sd_dreambooth_extension.git /sdui/stable-diffusion-webui/extensions/sd_dreambooth_extension
 pip install -r /sdui/stable-diffusion-webui/extensions/sd_dreambooth_extension/requirements.txt
 
 # chmod -R 777 /sdui
 
 echo "Dependencies Built and Installed"
-
-
