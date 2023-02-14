975a8f2c237c7c804c24034c94ab2c629b92943e
Ed Mosqueda (@bbaaxx) <bbaaxx@gmail.com>
2023-02-13 19:03:05 -0600

    fix wget

--- start.sh
+++ start.sh
@@ -3,18 +3,16 @@ echo "pod started"
 export PYTHONUNBUFFERED=1
 export GPG_TTY=$(tty)
 
-if [[ $PUBLIC_KEY ]]
-then
+if [[ $PUBLIC_KEY ]]; then
     mkdir -p ~/.ssh
     chmod 700 ~/.ssh
     cd ~/.ssh
-    echo $PUBLIC_KEY >> authorized_keys
+    echo $PUBLIC_KEY >>authorized_keys
     chmod 700 -R ~/.ssh
     cd /
 fi
 
-if [[ $JUPYTER_PASSWORD ]]
-then
+if [[ $JUPYTER_PASSWORD ]]; then
     cd /
     source /workspace/jupyter/.venv/bin/activate
     jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace
@@ -28,16 +26,15 @@ if [ ! -f /sdui/switch.off ]; then
         mkdir -p /workspace/local_ckpts
         ln -s /workspace/local_ckpts /workspace/stable-diffusion-webui/models/Stable-diffusion/
         echo "Downloading checkpoint"
-        wget --show-progress -P /workspace/local_ckpts
-        https://huggingface.co/ckpt/sd15/resolve/main/v1-5-pruned-emaonly.ckpt 
+        wget --show-progress -P /workspace/local_ckpts https://huggingface.co/ckpt/sd15/resolve/main/v1-5-pruned-emaonly.ckpt
     fi
     echo "Switch-off flag not found Launching WebUI"
 
-    if [[ $WEBUI == "invoke"  ]]; then
+    if [[ $WEBUI == "invoke" ]]; then
         echo "launching InvokeAi"
         source /sdui/invoke/.venv/bin/activate
         python relauncher.py &
-    elif [[ $WEBUI == "a111"  ]]; then
+    elif [[ $WEBUI == "a111" ]]; then
         echo "Launching A111 webui"
         source /sdui/stable-diffusion-webui/venv/bin/activate
         cd /sdui/stable-diffusion-webui
