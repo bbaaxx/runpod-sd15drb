c47d7863b1fc9307d7b2701acdecf333b89fbba5
Ed Mosqueda (@bbaaxx) <bbaaxx@gmail.com>
2023-02-15 18:07:08 -0600

    how about now ?

--- setup.sh
+++ setup.sh
@@ -5,23 +5,6 @@ echo "Starting Apps Setup"
 mkdir -p /workspace/local_ckpts # Mount point for checkpoints (on transient storage)
 mkdir -p /sdui/outputs          # Mount point for outputs (on persistent storage)
 
-# make symbolic links
-ln -s /sdui/invoke /workspace
-ln -s /sdui/stable-diffusion-webui /workspace
-ln -s /workspace/local_ckpts /workspace/stable-diffusion-webui/models/Stable-diffusion/
-ln -s /workspace/local_ckpts /workspace/invoke/models/InvokeAI/
-ln -s /workspace/stable-diffusion-webui/outputs /sdui/outputs/webui
-ln -s /workspace/invoke/outputs /sdui/outputs/invoke
-
-# Set permissions
-# chown -R poduser:poduser /workspace
-# chown -R poduser:poduser /sdui
-# chown poduser:poduser /start.sh
-
-// Enable write on created directories
-chmod -R 777 /workspace
-chmod -R 777 /sdui
-
 # Install JupyterLab as the user poduser
 # su - poduser -c "bash /user_apps.sh"
 cd /workspace
@@ -63,5 +46,22 @@ git clone https://github.com/camenduru/sd-webui-additional-networks.git /sdui/st
 git clone https://github.com/d8ahazard/sd_dreambooth_extension.git /sdui/stable-diffusion-webui/extensions/sd_dreambooth_extension
 pip install -r /sdui/stable-diffusion-webui/extensions/sd_dreambooth_extension/requirements.txt
 deactivate
+
+# make symbolic links
+ln -s /sdui/invoke /workspace
+ln -s /sdui/stable-diffusion-webui /workspace
+ln -s /workspace/local_ckpts /workspace/stable-diffusion-webui/models/Stable-diffusion/
+ln -s /workspace/local_ckpts /workspace/invoke/models/InvokeAI/
+ln -s /workspace/stable-diffusion-webui/outputs /sdui/outputs/webui
+ln -s /workspace/invoke/outputs /sdui/outputs/invoke
+
+# Set permissions
+# chown -R poduser:poduser /workspace
+# chown -R poduser:poduser /sdui
+# chown poduser:poduser /start.sh
+
+// Enable write on created directories
+chmod -R 777 /workspace
+chmod -R 777 /sdui
 
 echo "Apps Setup Complete"
