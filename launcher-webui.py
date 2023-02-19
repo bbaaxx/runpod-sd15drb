4c44102d91c61e3349f2e469ed22c7786378b2a8
Ed Mosqueda (@bbaaxx) <bbaaxx@gmail.com>
2023-02-19 09:42:38 -0600

    switch to launcher only

--- launcher-webui.py
+++ launcher-webui.py
@@ -1,7 +1,7 @@
 import os
 
 print('Launching  setup: Launching...')
-launch_string = "/workspace/stable-diffusion-webui/webui.sh --skip-cuda-check --xformers"
+launch_string = "/workspace/stable-diffusion-webui/launch.sh --skip-cuda-check"
 os.system(launch_string)
 print('Setup procedure complete')
 
