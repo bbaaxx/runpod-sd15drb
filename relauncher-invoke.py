fdbf711a6feb2b9f651ddbadcdc9452b7d1e7509
Ed Mosqueda (@bbaaxx) <bbaaxx@gmail.com>
2023-02-19 10:58:37 -0600

    Corrected /workspace route on launcher and venv for invoke

--- relauncher-invoke.py
+++ relauncher-invoke.py
@@ -5,7 +5,7 @@ while True:
     print('Relauncher: Launching...')
     if n > 0:
         print(f'\tRelaunch count: {n}')
-    launch_string = "invokeai --web --port 4206 --host 0.0.0.0"
+    launch_string = "source /workspace/invoke/.venv/bin/activate && invokeai --web --port 4206 --host 0.0.0.0"
     os.system(launch_string)
     print('Relauncher: Process is ending. Relaunching in 2s...')
     n += 1
--- relauncher-webui.py
+++ relauncher-webui.py
@@ -5,7 +5,7 @@ while True:
     print('Relauncher: Launching...')
     if n > 0:
         print(f'\tRelaunch count: {n}')
-    launch_string = "/workspace/stable-diffusion-webui/webui.sh -f"
+    launch_string = "/workspace/stable-diffusion-webui/webui.sh"
     os.system(launch_string)
     print('Relauncher: Process is ending. Relaunching in 2s...')
     n += 1
